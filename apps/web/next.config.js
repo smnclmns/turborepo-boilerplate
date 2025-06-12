93db3a3051da4544f66b43a5d25f202af8511dfb
smnclmns <simon2.clemens@tu-dortmund.de>
2025-06-12 15:23:13 +0200

    infra(Setup): remove Invalid next.config.js options

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    experimental: {
-        appDir: true,
-        serverActions: true,
-        // deaktiviert die Host/Origin-Validierung
-        serverActionsSkipHostCheck: true,
-    },
 }
 
 export default nextConfig;
